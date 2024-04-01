cc339436e83eca6b348fd9b694c89ca323f52f64
Claudio Contardo <claudio.contardo@concordia.ca>
2024-04-01 16:47:49 -0400

    modified cxxwrap signatures

--- wrapper/jlwrapper.cpp
+++ wrapper/jlwrapper.cpp
@@ -32,7 +32,7 @@ static void max_indep_set_cb(jlcxx::ArrayRef<int> _src, jlcxx::ArrayRef<int> _ds
     for(int i = 0; i < (int)mis.size(); ++ i) _sol[i] = mis[i];
     return;
 }
-JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
+JLCXX_MODULE define_julia_module_cb(jlcxx::Module& mod)
 {
   mod.method("max_indep_set_cb", &max_indep_set_cb);
 }
